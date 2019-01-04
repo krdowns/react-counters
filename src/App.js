14f60b57ca5e320cf3749b2289eb2b88cd291022
krdowns <krjdowns16@gmail.com>
2019-01-04 09:02:35 -0800

    finished

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
 import React, { Component } from "react";
 import Header from "./Header";
+import CounterList from "./CounterList"
 
 class App extends Component {
+
+  state = {
+    counters: 3
+  }
+
+  increaseCounters = () => {
+    let counter = this.state.counters + 1;
+    console.log("clicked")
+    this.setState({
+        counters: counter
+    });
+  }
+
+  decreaseCounters = () => {
+    let counter = this.state.counters - 1;
+
+    this.setState({
+        counters: counter
+    });
+  }
+
   render() {
     return (
       <div className="App">
-        <Header />
+        <Header decrease={this.decreaseCounters} increase={this.increaseCounters} />
+        <CounterList counter={this.state.counters} />
       </div>
     );
   }
+  
+  
 }
+
+
+
+export default App;
--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
+import ContainerList from './CounterList';
 
 class Header extends Component {
   render() {
     return (
       <header className="App-header">
         <h1 className="App-title">React Counters</h1>
-        <button>+</button>
-        <button>-</button>
+        <button onClick={this.props.decrease}>-</button>
+        <button onClick={this.props.increase}>+</button>
       </header>
     );
   }
